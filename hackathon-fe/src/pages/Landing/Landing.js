0eb8aa88bc7ff53cb410918f786881b4e9866cd8
Max Walker <maxwalker23@gmail.com>
2023-10-24 12:15:06 -0400

    landing page updates

--- hackathon-fe/src/pages/Landing/Landing.js
+++ hackathon-fe/src/pages/Landing/Landing.js
@@ -1,7 +1,21 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
+
+import styles from "./Landing.module.css";
+
+import heroImg from "../../assets/bday-hero-img.jpg";
 
 const Landing = () => {
-  return <h1>Lanidng Pg</h1>;
+  return (
+    <section className={styles["landing-container"]}>
+      <img src={heroImg} alt="Birthday Introduction" />
+      <div>
+        <h1>Lorem ipsum dolor consectetur</h1>
+        <p>lorem ipsum dolor consectetur lorem ipsum dolor consectetur lorem ipsum dolor consectetur,lorem ipsum dolor consectetur lorem ipsum dolor consectetur, lorem ipsum dolor consectetur.</p>
+      </div>
+      <NavLink to="/signup" className={styles.signup}>Sign Up</NavLink>
+      <NavLink className={styles.signup}>Log In</NavLink>
+    </section>
+  );
 };
 
 export default Landing;
--- hackathon-fe/src/pages/Login/Login.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Login = () => {
-  return <h1>Login pg</h1>;
-};
-
-export default Login;
