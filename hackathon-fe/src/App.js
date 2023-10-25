d69cb7670d3201705f207957109829936399a091
John McCants <47401369+johnmccants002@users.noreply.github.com>
2023-10-24 21:51:47 -0700

    Added filters page

--- hackathon-fe/src/App.js
+++ hackathon-fe/src/App.js
@@ -8,18 +8,19 @@ import LoginPage from "./pages/Login/Login";
 import ProfilePage from "./pages/Profile/Profile";
 import FriendsPage from "./pages/FriendsPage/FriendsPage";
 import FriendPage from "./pages/FriendPage/FriendPage";
-
+import FiltersPage from "./pages/FiltersPage/FiltersPage";
 function App() {
   return (
     <div className="App">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/friends" element={<FriendsPage />} />
-          <Route path="/friend/:id" element={<FriendPage />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/friends" element={<FriendsPage />} />
+        <Route path="/friend/:id" element={<FriendPage />} />
+        <Route path="/filters" element={<FiltersPage />} />
+      </Routes>
       <Footer />
     </div>
   );
