6c9e1e5a5b9a1649d750865519d09d53f12d8e36
Amber Taveras <taveras.amber@gmail.com>
2023-10-26 17:25:43 -0400

    implements friend photo upload for update friend

--- hackathon-fe/src/pages/UpdateFriendPage/UpdateFriendPage.js
+++ hackathon-fe/src/pages/UpdateFriendPage/UpdateFriendPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import { BsArrowLeft } from "react-icons/bs";
@@ -13,21 +13,45 @@ function UpdateFriendPage () {
   const navigate = useNavigate();
   const { id } = useParams();
 
-  const [friend, setFriend] = useState(null); 
   const [profileInput, setProfileInput] = useState(null);
-  const [dobObject, setDobObject] = useState(null);
+  const [displayFile, setDisplayFile] = useState(null);
+  const [uploadedFile, setUploadedFile] = useState(null);
+  const [buttonHTML, setButtonHTML] = useState('Add profile photo');
+
+  const fileInputRef = useRef(null);
 
   useEffect(() => {
     const fetchFriend = async () => {
       const friendInfo = await friendsService.showFriend(id);
-      setFriend(friendInfo);
       setProfileInput({
         ...friendInfo
       });
+      if (friendInfo.photo) {
+            setDisplayFile(friendInfo.photo);
+          setButtonHTML('Change photo');
+      }
     }
     fetchFriend();
   }, [id]);
 
+  function handleAddPhotoClick (evt) {
+    evt.preventDefault();
+    fileInputRef.current.click();
+}
+
+function handleFileChange (evt) {
+    // assigns file upload to display image, adds file to state for form submit, and toggles button HTML
+    const file = evt.target.files[0];
+    if (file) {
+        setDisplayFile(URL.createObjectURL(file));
+        setUploadedFile(file);
+        setButtonHTML('Change photo');
+    } else {
+        setDisplayFile(null);
+        setUploadedFile(null);
+        setButtonHTML('Add profile photo');
+    } 
+}
 
   const handleGiftTypeToggle = (type) => {
     const newGiftTypes = profileInput.giftPreferences.includes(type)
@@ -39,6 +63,11 @@ function UpdateFriendPage () {
   const submitHandler = async (e) => {
     e.preventDefault();
     const response = await friendsService.updateFriend(id, profileInput);
+    if (uploadedFile) {
+        const photoResponse = await friendsService.uploadPhoto(id, uploadedFile);
+        console.log(photoResponse);
+        if (photoResponse.ok && response.message === 'Friend updated') navigate((-1));
+    }
     if (response.message === 'Friend updated') navigate((-1));
 
   };
@@ -54,12 +83,15 @@ function UpdateFriendPage () {
           <h1>Edit Friend Profile</h1>
         </div>
 
-        <form onSubmit={submitHandler}>
+        <form onSubmit={submitHandler} encType="multipart/form-data">
           <div>
-            { profileInput && profileInput.photo }
-            <label htmlFor="image" className={styles["add-image"]}></label>
-            <input type="file" id="image" hidden />
-            <p>Add profile photo</p>
+            { displayFile ? (
+                    <img src={`${displayFile}`} alt="Uploaded" style={{ height: '80px', width: '80px', paddingBottom: '6px' }}/>
+                ) : (
+                    <label htmlFor="image" className={styles["add-image"]} >+</label>
+            )}
+            <input type="file" name="photo" ref={fileInputRef} onChange={handleFileChange} style={{ display: 'none' }} />
+            <button onClick={handleAddPhotoClick}>{buttonHTML}</button>
           </div>
           <br />
 
