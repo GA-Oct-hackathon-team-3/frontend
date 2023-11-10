2a25e95bcea77115d7eb41c2af92808a197e522c
Amber Taveras <taveras.amber@gmail.com>
2023-11-10 15:12:33 -0500

    supports and renders tag recs on type

--- hackathon-fe/src/pages/TagAdder/TagAdder.js
+++ hackathon-fe/src/pages/TagAdder/TagAdder.js
@@ -4,6 +4,8 @@ import { useParams, useNavigate, useLocation } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { debounce } from 'lodash';
+
 import * as friendsService from '../../utilities/friends-service';
 import * as tagService from '../../utilities/tags-service';
 
@@ -23,6 +25,7 @@ function TagAdder() {
   const [selectedTags, setSelectedTags] = useState([]);
   const [inputValue, setInputValue] = useState('');
   const [defaultTags, setDefaultTags] = useState([]);
+  const [suggestedTags, setSuggestedTags] = useState([]);
 
   useEffect(() => {
     const fetchFriend = async () => {
@@ -38,8 +41,21 @@ function TagAdder() {
     fetchTags();
   }, [id, tags]);
 
-  const handleInputChange = (e) => {
+  const fetchSuggestions = async (value) => {
+    if (value === '' || value.length < 3) return setSuggestedTags([]); // requires search term of 3 => characters
+    else {
+      const suggestions = await tagService.getSuggestions(value);
+      setSuggestedTags(suggestions);
+    }
+  };
+
+  const handleInputChange = async (e) => {
     setInputValue(e.target.value);
+    const delayedFetch = debounce(
+      () => fetchSuggestions(e.target.value.trim()),
+      300
+    ); // uses debounce to delay calls to backend (makes sure user has stopped typing)...
+    delayedFetch(); // then makes call to backend
   };
 
   const handleInputEnter = async () => {
@@ -49,6 +65,13 @@ function TagAdder() {
       setSelectedTags((prevSelectedTags) => [...prevSelectedTags, inputValue]);
       setInputValue('');
     }
+  };
+
+  const handleSuggestionClick = async (suggestion) => {
+    await tagService.addTag(id, suggestion);
+    setTags((prevTags) => [...prevTags, suggestion.title]);
+    setInputValue('');
+    setSuggestedTags([]);
   };
 
   const submitHandler = () => {
@@ -92,6 +115,21 @@ function TagAdder() {
             if (e.key === 'Enter') handleInputEnter();
           }}
         />
+
+        <div className={styles['suggestions-container']}>
+          <h3>Suggested Tags</h3>
+          {suggestedTags.length > 0
+            ? suggestedTags.map((suggestion) => (
+                <button
+                  className={styles['tag-button']}
+                  onClick={() => handleSuggestionClick(suggestion)}
+                  key={suggestion._id}
+                >
+                  {suggestion.title}
+                </button>
+              ))
+            : 'No suggestions'}
+        </div>
 
         <div className={styles['add-tags-container']}>
           <h3>Added Tags</h3>
