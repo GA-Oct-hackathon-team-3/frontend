3363af384f61e0c59fb3c83a7699454bfb4420dd
Amber Taveras <taveras.amber@gmail.com>
2023-11-12 00:55:21 -0500

    moves gift link formatting to helpers for multi use

--- hackathon-fe/src/components/ShowFriend/Gift.js
+++ hackathon-fe/src/components/ShowFriend/Gift.js
@@ -7,23 +7,13 @@ import { IconButton } from '@mui/material';
 import { BsHeart, BsHeartFill } from 'react-icons/bs';
 import { IconContext } from 'react-icons/lib';
 
+import { buildGiftLink } from '../../utilities/helpers';
+
 const Gift = ({ gift, isFavorite, location, toggleFavorite }) => {
   const [fillHeart, setFillHeart] = useState(isFavorite); // to toggle between empty or filled heart
 
-  const buildGiftLink = (gift) => {
-    if (/present/i.test(gift.giftType)) {
-      return `https://www.amazon.com/s?k=${gift.title}`;
-    } else if (/donation/i.test(gift.giftType)) {
-      return `https://www.google.com/search?q=${gift.title}`;
-    } else if (/experience/i.test(gift.giftType)) {
-      let query = `https://www.google.com/search?q=${gift.title}`;
-      if (location) query += `+near+${location}`; // uses friend location
-      return query;
-    }
-  };
-
   return (
-    <Link to={buildGiftLink(gift)} target="_blank" rel="noopener noreferrer">
+    <Link to={buildGiftLink(gift, location)} target="_blank" rel="noopener noreferrer">
       <div className={styles['product-pic']}>
         <img
           className={styles['product-pic']}
