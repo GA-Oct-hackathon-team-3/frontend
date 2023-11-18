01c4ee5a8208b573bb970fa16d1147e49aebc334
John McCants <47401369+johnmccants002@users.noreply.github.com>
2023-11-18 00:18:02 -0800

    update

--- hackathon-fe/src/utilities/profiles-service.js
+++ hackathon-fe/src/utilities/profiles-service.js
@@ -3,25 +3,25 @@ import { getToken } from "./users-service";
 import sendRequest from "./send-request";
 
 export async function getProfile() {
-    return await sendRequest(`${WEB_BASE_URL}/users/profile`, "GET", null);
+  return await sendRequest(`${WEB_BASE_URL}/users/profile`, "GET", null);
 }
 
 export async function updateUserProfile(userData) {
-    return await sendRequest(`${WEB_BASE_URL}/users/profile`, "PUT", userData);
+  return await sendRequest(`${WEB_BASE_URL}/users/profile`, "PUT", userData);
 }
 
 export async function uploadPhoto(file) {
-    const formData = new FormData();
-    formData.append('photo', file);
-    const token = getToken();
+  const formData = new FormData();
+  formData.append("photo", file);
+  const token = getToken();
 
-    const response = await fetch(`${WEB_BASE_URL}/users/profile/upload`, {
-        method: 'POST',
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-        body: formData,
-    });
+  const response = await fetch(`${WEB_BASE_URL}/users/profile/upload`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: formData,
+  });
 
-    if (response.status === 200) return response;
+  if (response.status === 200) return response;
 }
