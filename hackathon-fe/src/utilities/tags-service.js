9e97bfed4785cbe416e98914c8e93068649036c8
Amber Taveras <taveras.amber@gmail.com>
2023-11-19 02:45:10 -0500

    cahnges add/remove tags to update on submit

--- hackathon-fe/src/utilities/tags-service.js
+++ hackathon-fe/src/utilities/tags-service.js
@@ -2,28 +2,16 @@ import { WEB_BASE_URL } from "./constants";
 import sendRequest from "./send-request";
 const BASE_URL = `${WEB_BASE_URL}/`;
 
-export async function addTag(id, tag) {
-  const friend = await sendRequest(
-    BASE_URL + `friends/${id}/tags`,
-    "POST",
-    tag
-  );
-  return friend;
-}
-
 export async function getTags() {
   const friend = await sendRequest(BASE_URL + `tags`, "GET", null);
   return friend;
 }
 
-export async function removeTag(id, tagId) {
-  const friend = await sendRequest(
-    BASE_URL + `friends/${id}/tags/${tagId}`,
-    "DELETE",
-    null
-  );
-  return friend;
+export async function updateTags (id, tags) {
+    const response = await sendRequest(`${BASE_URL}friends/${id}/tags`, 'POST', tags);
+    return response;
 }
+
 export async function getSuggestions (query) {
     const suggestions = await sendRequest(`${BASE_URL}tags/suggestions?search=${query}`, 'GET', null);
     return suggestions;
