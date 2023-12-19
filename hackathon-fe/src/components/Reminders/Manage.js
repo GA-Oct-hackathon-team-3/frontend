e14871b96e36d6bf6ec8ee83cc07b489881d07d3
Amber Taveras <taveras.amber@gmail.com>
2023-12-19 01:56:17 -0500

    adds loading spinner

--- hackathon-fe/src/components/Reminders/Manage.js
+++ hackathon-fe/src/components/Reminders/Manage.js
@@ -6,42 +6,47 @@ import { formatDate } from '../../utilities/helpers';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faToggleOn, faToggleOff } from '@fortawesome/free-solid-svg-icons';
+import { CircularProgress } from '@mui/material';
 
 import styles from '../../styles/Reminders.module.css';
 
 const Manage = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [friends, setFriends] = useState([]); // use to reset filter
   const [filteredFriends, setFilteredFriends] = useState([]); // use to render
   const [updatedFriends, setUpdatedFriends] = useState({}); // accumulates changes to includeInNotifications per friend, using _id: boolean
   const [query, setQuery] = useState(''); // search query to filter friends in friend preferences
-
   const [frequency, setFrequency] = useState([]); // notificationSchedule preferences
 
+  const fetchUserProfile = async () => {
+    const profileInfo = await getProfile(); // fetch profile
+    if (profileInfo && profileInfo.profile) {
+      setFrequency(profileInfo.profile.notificationSchedule); // initialize user's current preferences
+    }
+  };
+
+  const fetchFriends = async () => {
+    const friendsData = await retrieveFriends(); // fetch friends
+    if (friendsData) {
+      // friends comes in { today: [], thisWeek: [], thisMonth: [], laterOn: [] } structure
+      // use concat to flatten into single array
+      const flattenedFriendArray = friendsData.today.concat(
+        friendsData.thisWeek,
+        friendsData.thisMonth,
+        friendsData.laterOn
+      );
+      setFriends(flattenedFriendArray); // maintains all friends (to revert changes from query)
+      setFilteredFriends(flattenedFriendArray); // displays data (is updated with query)
+    }
+  };
+
   useEffect(() => {
-    const fetchUserProfile = async () => {
-      const profileInfo = await getProfile(); // fetch profile
-      if (profileInfo && profileInfo.profile) {
-        setFrequency(profileInfo.profile.notificationSchedule); // initialize user's current preferences
-      }
-    };
-
-    const fetchFriends = async () => {
-      const friendsData = await retrieveFriends(); // fetch friends
-      if (friendsData) {
-        // friends comes in { today: [], thisWeek: [], thisMonth: [], laterOn: [] } structure
-        // use concat to flatten into single array
-        const flattenedFriendArray = friendsData.today.concat(
-          friendsData.thisWeek,
-          friendsData.thisMonth,
-          friendsData.laterOn
-        );
-        setFriends(flattenedFriendArray); // maintains all friends (to revert changes from query)
-        setFilteredFriends(flattenedFriendArray); // displays data (is updated with query)
-      }
-    };
-
     fetchUserProfile();
     fetchFriends();
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1200);
   }, []);
 
   const handleSearch = (query) => {
@@ -68,30 +73,40 @@ const Manage = () => {
 
   const handleSelectFriend = (id, includeInNotifications) => {
     // if if is a key in object, set to opposite value, otherwise set to !includeNotifications
-    const updatedNotificationValue = updatedFriends.hasOwnProperty(id) ? !updatedFriends[id] : !includeInNotifications;
+    const updatedNotificationValue = updatedFriends.hasOwnProperty(id)
+      ? !updatedFriends[id]
+      : !includeInNotifications;
 
     // maintains object of key with ids of friends were who updated, and the boolean value to which includeInNotifications should be updated to
     // ids for submission to backend, value to update render
     setUpdatedFriends((prev) => {
-        if (updatedNotificationValue === includeInNotifications) { // if the value to update is equal to original value (i.e if toggled twice)...
-            const { [id]: omit, ...rest } = prev; // remove id key from state to prevent submission of id to backend
-            return rest;
-        }
+      if (updatedNotificationValue === includeInNotifications) {
+        // if the value to update is equal to original value (i.e if toggled twice)...
+        const { [id]: omit, ...rest } = prev; // remove id key from state to prevent submission of id to backend
+        return rest;
+      }
 
-        return {
-            ...prev,
-            [id]: updatedNotificationValue
-        }
+      return {
+        ...prev,
+        [id]: updatedNotificationValue,
+      };
     });
   };
 
   const handleFrequencySubmit = async (evt) => {
     evt.preventDefault();
     if (frequency && frequency.length > 0) {
-        const userData = {
-            notificationSchedule: [...frequency]
-        }
-        const response = await updateUserProfile(userData);
+      const userData = {
+        notificationSchedule: [...frequency],
+      };
+      const response = await updateUserProfile(userData);
+      if (response && response.message === 'User profile updated') {
+        setIsLoading(true); // set loading to true to simulate loading
+        setTimeout(() => {
+          // time out to set to false and render page
+          setIsLoading(false);
+        }, 1200);
+      }
     }
   };
 
@@ -100,7 +115,15 @@ const Manage = () => {
 
     const friendIds = Object.keys(updatedFriends);
     const response = await updateFriendNotification(friendIds);
-    if (response.message === 'Updated friend notification preference successfully'); setUpdatedFriends({});
+    if (response.message === 'Updated friend notification preference successfully') {
+      setUpdatedFriends({}); // resets state
+      fetchFriends(); // re-fetches friends to retrieve updated friend data
+      setIsLoading(true); // set loading to true as data is retrieved
+      setTimeout(() => {
+        // time out to set to false and render page
+        setIsLoading(false);
+      }, 1200);
+    }
   };
 
   // reusable render of checkbox for freqnecy preferences section
@@ -113,83 +136,120 @@ const Manage = () => {
         checked={frequency.includes(value)}
         onChange={() => handleCheckbox(value)}
       />
-      <label htmlFor={label} className={frequency.includes(value) ? styles['custom-checkbox-checked'] : styles['custom-checkbox']}>{frequency.includes(value) ? '\u2714' : ''}</label>
-    <label htmlFor={label}>{label}</label>
+      <label
+        htmlFor={label}
+        className={
+          frequency.includes(value)
+            ? styles['custom-checkbox-checked']
+            : styles['custom-checkbox']
+        }
+      >
+        {frequency.includes(value) ? '\u2714' : ''}
+      </label>
+      <label htmlFor={label}>{label}</label>
     </div>
   );
 
   return (
     <div className={styles['tab-section']}>
-      <div className={styles['preference-section']}>
-        <h3>Frequency Preferences</h3>
-        <p>
-          Select the number of days in advance you want to receive a
-          notification. Please choose at least one option.
-        </p>
-
-        <form onSubmit={handleFrequencySubmit} className={styles['preference-form']}>
-          {renderFrequencyCheckbox(30, 'One Month')}
-          {renderFrequencyCheckbox(7, 'One Week')}
-          {renderFrequencyCheckbox(0, 'Day of')}
-
-          <input type="submit" value="Confirm Frequency Preferences" className={styles['update-button']} />
-        </form>
-      </div>
-
-      <div className={styles['preference-section']}>
-          <h3>Friend Preferences</h3>
-          <p>Enable or disable notifications for each friend and confirm below:</p>
-        <input
-          value={query}
-          onChange={(evt) => handleSearch(evt.target.value)}
-          placeholder="Search by name..."
-          className={styles['search-bar']}
-        />
-        <div className={styles['friend-section']}>
-          {filteredFriends &&
-            filteredFriends.map((friend) => {
-              return (
-                <div key={friend._id} className={styles['friend-item']}>
-                  <img
-                    src={
-                      friend.photo
-                        ? friend.photo
-                        : 'https://i.imgur.com/hCwHtRc.png'
-                    }
-                    alt={friend.name}
-                  />
-                  <div className={styles['friend-info']}>
-                  <p style={{ fontWeight: 'bold' }}>{friend.name}</p>
-                  <p>|</p>
-                  <p>{formatDate(friend.dob)}</p>
-                  </div>
-                  <div
-                    onClick={() =>
-                      handleSelectFriend(
-                        friend._id,
-                        friend.includeInNotifications
-                      )
-                    }
-                  >
-                    {
-                      // if friend is in updatedFriends, display new value, else display includeInNotifications value
-                      updatedFriends[friend._id] ??
-                      friend.includeInNotifications ? (
-                        <FontAwesomeIcon icon={faToggleOn} color="#53CF85" className={styles['friend-toggle']} />
-                      ) : (
-                        <FontAwesomeIcon icon={faToggleOff} color="#AF95E7" className={styles['friend-toggle']} />
-                      )
-                    }
-                  </div>
-                </div>
-              );
-            })}
-
-          <button onClick={(evt) => handleFriendSubmit(evt)} className={styles['update-button']}>
-            Confirm Friend Preferences
-          </button>
+      {isLoading ? (
+        <div style={{ margin: 'auto', marginTop: '20%' }}>
+          <CircularProgress color="secondary" />
         </div>
-      </div>
+      ) : (
+        <>
+          <div className={styles['preference-section']}>
+            <h3>Frequency Preferences</h3>
+            <p>
+              Select the number of days in advance you want to receive a
+              notification. Please choose at least one option.
+            </p>
+
+            <form
+              onSubmit={handleFrequencySubmit}
+              className={styles['preference-form']}
+            >
+              {renderFrequencyCheckbox(30, 'One Month')}
+              {renderFrequencyCheckbox(7, 'One Week')}
+              {renderFrequencyCheckbox(0, 'Day of')}
+
+              <input
+                type="submit"
+                value="Confirm Frequency Preferences"
+                className={styles['update-button']}
+              />
+            </form>
+          </div>
+
+          <div className={styles['preference-section']}>
+            <h3>Friend Preferences</h3>
+            <p>
+              Enable or disable notifications for each friend and confirm below:
+            </p>
+            <input
+              value={query}
+              onChange={(evt) => handleSearch(evt.target.value)}
+              placeholder="Search by name..."
+              className={styles['search-bar']}
+            />
+            <div className={styles['friend-section']}>
+              {filteredFriends &&
+                filteredFriends.map((friend) => {
+                  return (
+                    <div key={friend._id} className={styles['friend-item']}>
+                      <img
+                        src={
+                          friend.photo
+                            ? friend.photo
+                            : 'https://i.imgur.com/hCwHtRc.png'
+                        }
+                        alt={friend.name}
+                      />
+                      <div className={styles['friend-info']}>
+                        <p style={{ fontWeight: 'bold' }}>{friend.name}</p>
+                        <p>|</p>
+                        <p>{formatDate(friend.dob)}</p>
+                      </div>
+                      <div
+                        onClick={() =>
+                          handleSelectFriend(
+                            friend._id,
+                            friend.includeInNotifications
+                          )
+                        }
+                      >
+                        {
+                          // if friend is in updatedFriends, display new value, else display includeInNotifications value
+                          updatedFriends[friend._id] ??
+                          friend.includeInNotifications ? (
+                            <FontAwesomeIcon
+                              icon={faToggleOn}
+                              color="#53CF85"
+                              className={styles['friend-toggle']}
+                            />
+                          ) : (
+                            <FontAwesomeIcon
+                              icon={faToggleOff}
+                              color="#AF95E7"
+                              className={styles['friend-toggle']}
+                            />
+                          )
+                        }
+                      </div>
+                    </div>
+                  );
+                })}
+
+              <button
+                onClick={(evt) => handleFriendSubmit(evt)}
+                className={styles['update-button']}
+              >
+                Confirm Friend Preferences
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- hackathon-fe/src/components/Reminders/Notifications.js
+++ hackathon-fe/src/components/Reminders/Notifications.js
@@ -6,10 +6,12 @@ import { getAgeAndSuffix } from '../../utilities/helpers';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { CircularProgress } from '@mui/material';
 
 import styles from '../../styles/Reminders.module.css';
 
 const Notifications = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [notifications, setNotifications] = useState({ current: [], past: [] });
 
   useEffect(() => {
@@ -20,15 +22,19 @@ const Notifications = () => {
     };
 
     const readNotifications = async () => {
-        const ids = notifications.current.map(notif => notif._id); // creates array of all unread notifications ids
-        const response = await markAsRead(ids); // sends to backend to mark as read
-    }
+      const ids = notifications.current.map((notif) => notif._id); // creates array of all unread notifications ids
+      const response = await markAsRead(ids); // sends to backend to mark as read
+    };
 
     fetchNotifications();
 
-    setTimeout(() => { 
-        // if no unread notifications, no call to backend
-        if (notifications.current.length > 0) readNotifications();
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1200);
+
+    setTimeout(() => {
+      // if no unread notifications, no call to backend
+      if (notifications.current.length > 0) readNotifications();
     }, 3000); // runs 3 seconds after mount
   }, []);
 
@@ -41,22 +47,24 @@ const Notifications = () => {
     photo,
     setNotifications,
   }) => {
-
     const navigate = useNavigate();
 
     const removeReminder = async () => {
-        const response = await deleteNotification(id);
-        if (response && response.message === 'Notification deleted successfully') {
-            setNotifications((prev) => ({
-                ...prev,
-                current: prev.current.filter(notif => notif._id !==id),
-                past: prev.past.filter(notif => notif._id !==id),
-            }));
-        }
+      const response = await deleteNotification(id);
+      if (response && response.message === 'Notification deleted successfully') {
+        setNotifications((prev) => ({
+          ...prev,
+          current: prev.current.filter((notif) => notif._id !== id),
+          past: prev.past.filter((notif) => notif._id !== id),
+        }));
       }
+    };
 
     return (
-      <div className={styles['notification-item']} onClick={() => navigate(`/friend/${friendId}`)}>
+      <div
+        className={styles['notification-item']}
+        onClick={() => navigate(`/friend/${friendId}`)}
+      >
         <div className={styles['notification-card']}>
           <img
             src={photo ? photo : 'https://i.imgur.com/hCwHtRc.png'}
@@ -70,12 +78,12 @@ const Notifications = () => {
             <p>{Math.abs(daysUntilBirthday)}</p>
             <p>{daysUntilBirthday >= 0 ? 'Days Left' : 'Days Ago'}</p>
           </div>
-        <FontAwesomeIcon
-                icon={faTimesCircle}
-                className={styles['notification-close']}
-                color='#53CF85'
-                onClick={removeReminder}
-                />
+          <FontAwesomeIcon
+            icon={faTimesCircle}
+            className={styles['notification-close']}
+            color="#53CF85"
+            onClick={removeReminder}
+          />
         </div>
       </div>
     );
@@ -83,38 +91,46 @@ const Notifications = () => {
 
   return (
     <div className={styles['tab-section']}>
-      <div className={styles['notification-section']}>
-        <h3>Current</h3>
-        {notifications.current &&
-          notifications.current.map((item) => (
-            <NotificationItem
-              key={item._id.toString()}
-              id={item._id.toString()}
-              friendId={item.friend._id}
-              name={item.friend.name}
-              dob={item.friend.dob}
-              daysUntilBirthday={item.friend.daysUntilBirthday}
-              photo={item.friend.photo}
-              setNotifications={setNotifications}
-            />
-          ))}
-      </div>
-      <div className={styles['notification-section']}>
-        <h3>Past</h3>
-        {notifications.past &&
-          notifications.past.map((item) => (
-            <NotificationItem
-              key={item._id.toString()}
-              id={item._id.toString()}
-              friendId={item.friend._id}
-              name={item.friend.name}
-              dob={item.friend.dob}
-              daysUntilBirthday={item.friend.daysUntilBirthday}
-              photo={item.friend.photo}
-              setNotifications={setNotifications}
-            />
-          ))}
-      </div>
+      {isLoading ? (
+        <div style={{ margin: 'auto', marginTop: '20%' }}>
+          <CircularProgress color="secondary" />
+        </div>
+      ) : (
+        <>
+          <div className={styles['notification-section']}>
+            <h3>Current</h3>
+            {notifications.current &&
+              notifications.current.map((item) => (
+                <NotificationItem
+                  key={item._id.toString()}
+                  id={item._id.toString()}
+                  friendId={item.friend._id}
+                  name={item.friend.name}
+                  dob={item.friend.dob}
+                  daysUntilBirthday={item.friend.daysUntilBirthday}
+                  photo={item.friend.photo}
+                  setNotifications={setNotifications}
+                />
+              ))}
+          </div>
+          <div className={styles['notification-section']}>
+            <h3>Past</h3>
+            {notifications.past &&
+              notifications.past.map((item) => (
+                <NotificationItem
+                  key={item._id.toString()}
+                  id={item._id.toString()}
+                  friendId={item.friend._id}
+                  name={item.friend.name}
+                  dob={item.friend.dob}
+                  daysUntilBirthday={item.friend.daysUntilBirthday}
+                  photo={item.friend.photo}
+                  setNotifications={setNotifications}
+                />
+              ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
