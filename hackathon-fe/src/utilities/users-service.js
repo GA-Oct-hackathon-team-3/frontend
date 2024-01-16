f8e74119f6196cc29ea7e34ceeabc0fe4d2a0e2c
Amber Taveras <taveras.amber@gmail.com>
2024-01-16 16:28:56 -0500

    updates getToken to auto call refresh token if expired token

--- hackathon-fe/src/utilities/users-service.js
+++ hackathon-fe/src/utilities/users-service.js
@@ -20,15 +20,21 @@ export async function resetPassword (passwordData, token) {
     return await usersAPI.resetPassword(passwordData, token);
 }
 
-export function getToken() {
+export async function getToken() {
+    // get token from local storage
   const token = localStorage.getItem('token');
-  if (!token) return null;
+  // if no token, log user out
+  if (!token) return logOut();
+  // parse token
   const payload = JSON.parse(atob(token.split('.')[1]));
-  if (payload.exp < Date.now() / 1000) {
-    localStorage.removeItem('token');
-    return null;
-  }
-  return token;
+  // if token is expired, refresh the token and return the new token
+  if (payload.exp < Date.now() / 1000) return await refreshTokens('web', token);
+  // else return the token
+  else return token;
+}
+
+export async function refreshTokens (device, token) {
+    return await usersAPI.refreshTokens(device, token);
 }
 
 export async function login(credentials) {
@@ -52,8 +58,8 @@ export function getUser() {
   return token ? userData : null;
 }
 
-export function logOut() {
-  localStorage.removeItem('token');
+export async function logOut() {
+    return await usersAPI.logout();
 }
 
 export async function deleteUser() {
